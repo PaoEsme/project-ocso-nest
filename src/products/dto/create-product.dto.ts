9db840342158ee41bf6eee8fe079ca713354f742
PaoEsme <paesvero1983@gmail.com>
2024-10-07 15:46:19 -0600

    Added products CRUD

--- src/products/dto/create-product.dto.ts
+++ src/products/dto/create-product.dto.ts
@@ -1,1 +1,7 @@
-export class CreateProductDto {}
+export class CreateProductDto {
+    productId: string;
+    productName: string;
+    price: number;
+    countSeal: number;
+    provider: string;
+}
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,26 +1,66 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { v4 as uuid } from 'uuid';
 
 @Injectable()
 export class ProductsService {
+  private products: CreateProductDto[] = [
+    {
+      productId: uuid(),
+      productName: "Sabritas Normal 60g",
+      price: 29,
+      countSeal: 3,
+      provider: uuid(),
+    },
+    {
+      productId: uuid(),
+      productName: "Coca Cola 600ml",
+      price: 40,
+      countSeal: 2,
+      provider: uuid(),
+    },
+    {
+      productId: uuid(),
+      productName: "Agua Ciel 1L",
+      price: 15,
+      countSeal: 2,
+      provider: uuid(),
+    },
+  ]
   create(createProductDto: CreateProductDto) {
-    return 'This action adds a new product';
+    createProductDto.productId = uuid();
+    this.products.push(createProductDto)
+    return createProductDto; 
   }
 
   findAll() {
-    return `This action returns all products`;
+    return this.products;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} product`;
+  findOne(id: string) {
+    const productFound = this.products.filter((product)=>product.productId == id)[0]
+    if (!productFound) throw new NotFoundException()
+    return productFound;
   }
 
-  update(id: number, updateProductDto: UpdateProductDto) {
-    return `This action updates a #${id} product`;
+  findByProvider(id: string) {
+    const productsFound = this.products.filter((product)=>product.productId == id)
+    if (productsFound.length == 0) throw new NotFoundException()
+    return productsFound;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} product`;
+  update(id: string, updateProductDto: UpdateProductDto) {
+    let product = this.findOne(id)
+    return {
+      ...product,
+      ...updateProductDto,
+    }
+  }
+
+  remove(id: string) {
+    const { productId } = this.findOne(id)
+    this.products = this.products.filter((product) => product.productId != productId)
+    return this.products;
   }
 }
