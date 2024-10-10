a99a510e988905df29e5a2e0f18a3f24d6cd560f
PaoEsme <paesvero1983@gmail.com>
2024-10-10 01:01:44 -0600

    Manager and Regions setup

--- src/locations/locations.service.ts
+++ src/locations/locations.service.ts
@@ -1,11 +1,14 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 import { CreateLocationDto } from './dto/create-location.dto';
 import { UpdateLocationDto } from './dto/update-location.dto';
-import { Repository } from 'typeorm';
+import { Location } from './entities/location.entity';
 
 @Injectable()
 export class LocationsService {
   constructor(
+    @InjectRepository(Location)
     private locationRepository: Repository<Location>
   ){}
   create(createLocationDto: CreateLocationDto) {
@@ -23,17 +26,17 @@ export class LocationsService {
     if (!location) throw new NotFoundException("Location not found")
   }
 
-  update(id: number, updateLocationDto: UpdateLocationDto) {
-    const location = this.locationRepository.preload({
+  async update(id: number, updateLocationDto: UpdateLocationDto) {
+    const location = await this.locationRepository.preload({
       locationId: id,
       ...updateLocationDto,
     })
-    return location
+    return this.locationRepository.save(location)
   }
 
   remove(id: number) {
     return this.locationRepository.delete({
-      locationId: id;
+      locationId: id
     })
   }
 }
