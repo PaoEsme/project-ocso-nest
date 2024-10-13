6121a813ec0e6027f8099db76f26c5b514966d23
PaoEsme <paesvero1983@gmail.com>
2024-10-13 10:13:31 -0600

    Initial auth implementation

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,10 +5,11 @@ import { AppService } from "./app.service";
 import { EmployeesModule } from "./employees/employees.module";
 import { ProductsModule } from "./products/products.module";
 import { ConfigModule } from "@nestjs/config";
-import { ProvidersModule } from './providers/providers.module';
-import { ManagersModule } from './managers/managers.module';
-import { LocationsModule } from './locations/locations.module';
-import { RegionsModule } from './regions/regions.module';
+import { ProvidersModule } from "./providers/providers.module";
+import { ManagersModule } from "./managers/managers.module";
+import { LocationsModule } from "./locations/locations.module";
+import { RegionsModule } from "./regions/regions.module";
+import { AuthModule } from "./auth/auth.module";
 
 @Module({
   imports: [
@@ -24,7 +25,7 @@ import { RegionsModule } from './regions/regions.module';
       synchronize: true,
     }),
     EmployeesModule, 
-    ProductsModule, ProvidersModule, ManagersModule, LocationsModule, RegionsModule,
+    ProductsModule, ProvidersModule, ManagersModule, LocationsModule, RegionsModule, AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
