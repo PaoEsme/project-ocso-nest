52843978342378d78d99c6f207ee09dccc13a807
PaoEsme <paesvero1983@gmail.com>
2024-11-14 22:34:39 -0600

    feat: cookies in back

--- src/auth/decorators/auth.decorator.ts
+++ src/auth/decorators/auth.decorator.ts
@@ -1,7 +1,7 @@
 import { applyDecorators, UseGuards } from "@nestjs/common";
 import { Roles } from "./roles.decorator";
-import { AuthGuard } from "src/guards/auth.guard";
-import { RolesGuard } from "src/guards/roles.guard";
+import { AuthGuard } from "src/auth/guards/auth.guard";
+import { RolesGuard } from "src/auth/guards/roles.guard";
 import { ROLES } from "../constants/roles.constants";
 
 export const Auth = (...roles: ROLES[]) => {
